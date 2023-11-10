ef79c051cdb32494e757982c0e8b8191b0258c8c
paolovsantos <paolovsantos@gmail.com>
2023-11-10 02:34:47 +0000

    lab 11 python program

---
+++ psantos11.py
@@ -0,0 +1,9 @@
+# Paolo Santos
+# CSC 321
+# 11/9/2023
+# Lab #11
+
+num = int(input("Enter a number: "))
+print("Your number squared is: ",num*num)
+
+print("\nP.S. \"Sucking at something is the first step towards being sorta good at something.\"- JAKE THE DOG\n")
